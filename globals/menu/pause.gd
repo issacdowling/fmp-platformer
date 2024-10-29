ce8cbe20b24ecd334b881c872f784015442e562f
issacdowling <git@issacdowling.com>
2024-10-29 14:11:37 +0000

    Add ability to switch renderer (OpenGL or Vulkan Desktop/Mobile), fix swapped FSR1/2 menu options

--- globals/menu/pause.gd
+++ globals/menu/pause.gd
@@ -1,20 +1,26 @@
 extends Control
 
-const GLOBAL_ILLUMINATION = "global_illumination"
-const SCALING_METHOD = "scaling_method"
-const SCALING_AMOUNT = "scaling_amount"
+const GLOBAL_ILLUMINATION: String = "global_illumination"
+const SCALING_METHOD: String = "scaling_method"
+const SCALING_AMOUNT: String = "scaling_amount"
+const RENDERER: String = "renderer"
 
-const SCALING_METHODS_FSR1 = "FSR 2"
-const SCALING_METHODS_FSR2 = "FSR 1"
-const SCALING_METHODS_BILINEAR = "Bilinear"
+const SCALING_METHODS_FSR2: String = "FSR 2"
+const SCALING_METHODS_FSR1: String = "FSR 1"
+const SCALING_METHODS_BILINEAR: String = "Bilinear"
+
+const RENDERER_ADVANCED: String = "Advanced (Forward+)"
+const RENDERER_BASIC: String = "Basic (Mobile)"
+const RENDERER_COMPATIBILITY: String = "Compatibility (OpenGL)"
 
 signal setting_changed
 
-@onready var SettingSetter: Node = $SettingSetter
+@onready var SettingSetter: Node = %SettingSetter
 
-@onready var GlobalIlluminationToggle: CheckButton = $GlobalIlluminationToggle
-@onready var ScalingOptionsDropdown: OptionButton = $ScalingOptions
-@onready var ScalingAmountSlider: HSlider = $ScalingAmount
+@onready var GlobalIlluminationToggle: CheckButton = %GlobalIlluminationToggle
+@onready var ScalingOptionsDropdown: OptionButton = %ScalingOptions
+@onready var ScalingAmountSlider: HSlider = %ScalingAmount
+@onready var RendererOptionsDropdown: OptionButton = %RendererOptions
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,11 +29,17 @@ func _ready() -> void:
 	GlobalIlluminationToggle.toggled.connect(_on_global_illumination_toggle_toggled)
 	ScalingOptionsDropdown.item_selected.connect(_on_scaling_options_item_selected)
 	ScalingAmountSlider.value_changed.connect(_on_scaling_amount_value_changed)
+	RendererOptionsDropdown.item_selected.connect(_on_renderer_options_item_selected)
 	
 	# Add all scaling options
 	ScalingOptionsDropdown.add_item(SCALING_METHODS_FSR2)
 	ScalingOptionsDropdown.add_item(SCALING_METHODS_FSR1)
 	ScalingOptionsDropdown.add_item(SCALING_METHODS_BILINEAR)
+	
+	# Add all rendering options
+	RendererOptionsDropdown.add_item(RENDERER_ADVANCED)
+	RendererOptionsDropdown.add_item(RENDERER_BASIC)
+	RendererOptionsDropdown.add_item(RENDERER_COMPATIBILITY)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -46,3 +58,6 @@ func _on_scaling_options_item_selected(index: int) -> void:
 
 func _on_scaling_amount_value_changed(value: float) -> void:
 	setting_changed.emit(SCALING_AMOUNT, value)
+	
+func _on_renderer_options_item_selected(index: int) -> void:
+	setting_changed.emit(RENDERER, RendererOptionsDropdown.get_item_text(index))
--- globals/menu/setting_setter.gd
+++ globals/menu/setting_setter.gd
@@ -3,10 +3,12 @@ extends Node
 var current_settings: Dictionary = {}
 var json: JSON = JSON.new()
 const SETTINGS_FILE_PATH: String = "user://settings.json"
+const PROJECT_OVERRIDE_PATH: String = "user://project_overrides.cfg"
 
-@onready var GlobalIlluminationToggle: CheckButton = $"../GlobalIlluminationToggle"
-@onready var ScalingOptionsDropdown: OptionButton = $"../ScalingOptions"
-@onready var ScalingAmountSlider: HSlider = $"../ScalingAmount"
+@onready var GlobalIlluminationToggle: CheckButton = %GlobalIlluminationToggle
+@onready var ScalingOptionsDropdown: OptionButton = %ScalingOptions
+@onready var ScalingAmountSlider: HSlider = %ScalingAmount
+@onready var RendererOptionsDropdown: OptionButton = %RendererOptions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await Pause.ready # Ensure all setting things are prepared before changing.
@@ -30,22 +32,13 @@ func _ready() -> void:
 		# Emitting using another node's emitter rather than using this function directly because other nodes
 		# need to be aware of these changes
 		Pause.setting_changed.emit(setting, current_settings[setting])
-		match setting:
-			Pause.GLOBAL_ILLUMINATION:
-				GlobalIlluminationToggle.button_pressed = bool(current_settings[setting])
-			Pause.SCALING_METHOD:
-				for index in range(ScalingOptionsDropdown.item_count):
-					if ScalingOptionsDropdown.get_item_text(index) == current_settings[setting]:
-						ScalingOptionsDropdown.select(index)
-				#ScalingOptionsDropdown.select(int(current_settings[setting])) #FIX ME LATER!!!
-			Pause.SCALING_AMOUNT:
-				ScalingAmountSlider.value = float(current_settings[setting])
 
 # warnings-disable
 func _change_setting(setting: String, value: Variant) -> void:
 	current_settings[setting] = value
 	var settings_file: FileAccess = FileAccess.open(SETTINGS_FILE_PATH, FileAccess.WRITE)
 	settings_file.store_string(JSON.stringify(current_settings))
+	settings_file.close()
 	
 	print(setting, " set to ", value)
 	match setting:
@@ -61,7 +54,53 @@ func _change_setting(setting: String, value: Variant) -> void:
 					get_viewport().set_scaling_3d_mode(Viewport.SCALING_3D_MODE_BILINEAR)
 		Pause.SCALING_AMOUNT:
 			get_viewport().set_scaling_3d_scale(value as float)
+		Pause.RENDERER:
+			var config_override: ConfigFile = ConfigFile.new()
+			if value != Pause.RENDERER_ADVANCED:
+				# If the renderer doesn't support these features, but they're on, alert the user and disable them.
+				if (current_settings[Pause.GLOBAL_ILLUMINATION] == true || current_settings[Pause.SCALING_METHOD] != Pause.SCALING_METHODS_BILINEAR):
+					Toast.make_timed_toast("Non-advanced renderers don't support FSR or Global Illumination.", 7)
+					Pause.setting_changed.emit(Pause.SCALING_METHOD, Pause.SCALING_METHODS_BILINEAR)
+					Pause.setting_changed.emit(Pause.GLOBAL_ILLUMINATION, false)				
+				
+				GlobalIlluminationToggle.disabled = true
+				ScalingOptionsDropdown.disabled = true
+			else:
+				GlobalIlluminationToggle.disabled = false
+				ScalingOptionsDropdown.disabled = false
+				
+			config_override.load(PROJECT_OVERRIDE_PATH)
+
+			var selected_renderer: String = "gl_compatibility" # If somehow this fails, fall back on the most basic option.
+			match value:
+				Pause.RENDERER_ADVANCED:
+					selected_renderer = "forward_plus"
+				Pause.RENDERER_BASIC:
+					selected_renderer = "mobile"
+				Pause.RENDERER_COMPATIBILITY:
+					selected_renderer = "gl_compatibility"
+			# Need both, since mobile will be applied if on mobile, and regular on desktop
+			config_override.set_value("rendering", "renderer/rendering_method.mobile", selected_renderer)
+			config_override.set_value("rendering", "renderer/rendering_method", selected_renderer)
 			
+			config_override.save(PROJECT_OVERRIDE_PATH)
+			
+	# Although changing these through the UI makes the UI match the settings,
+	# changes done otherwise do not affect the UI by default, so I make sure they do.
+	match setting:
+		Pause.GLOBAL_ILLUMINATION:
+			GlobalIlluminationToggle.button_pressed = bool(current_settings[setting])
+		Pause.SCALING_METHOD:
+			for index in range(ScalingOptionsDropdown.item_count):
+				if ScalingOptionsDropdown.get_item_text(index) == current_settings[setting]:
+					ScalingOptionsDropdown.select(index)
+			#ScalingOptionsDropdown.select(int(current_settings[setting])) #FIX ME LATER!!!
+		Pause.SCALING_AMOUNT:
+			ScalingAmountSlider.value = float(current_settings[setting])
+		Pause.RENDERER:
+			for index in range(RendererOptionsDropdown.item_count):
+				if RendererOptionsDropdown.get_item_text(index) == current_settings[setting]:
+					RendererOptionsDropdown.select(index)
 	print(current_settings)
 
 		
