3cc7afc7eb79739340250b6d3ff408ad1861ce5c
issacdowling <git@issacdowling.com>
2024-12-04 09:34:42 +0000

    Update player animations to be less sassy

--- player/player.gd
+++ player/player.gd
@@ -2,10 +2,14 @@ extends CharacterBody3D
 
 
 @export var WALK_SPEED: float = 5.0
-@export var JUMP_VELOCITY: float = 6.5
+@export var SPRINT_MULTIPLIER: float = 1.6
+@export var JUMP_VELOCITY: float = 5.5
 
 @export var MOUSE_LOOK_SENSITIVITY: int = 1
 @export var CONTROLLER_LOOK_SENSITIVITY: int = 1
+
+var jump_time: float = 0
+var current_walk_speed: float
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -15,8 +19,11 @@ func _physics_process(delta: float) -> void:
 
 func move(delta: float) -> void:
 	# Add gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if not is_on_floor() and not is_on_wall():
+		jump_time += delta / 1.5
+		velocity += (get_gravity() * delta) + (get_gravity() * jump_time * 0.25)
+	else:
+		jump_time = 0
 		
 	# Get input, and move by input vector multiplied by the current transform	
 	var input_dir := Input.get_vector("left", "right", "forwards", "backwards")
@@ -24,9 +31,16 @@ func move(delta: float) -> void:
 	var move_vector := (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
 	if move_vector:
 		$AnimationTree.set("parameters/conditions/idle", false)
-		$AnimationTree.set("parameters/conditions/walk", true)
-		velocity.z = move_vector.z * WALK_SPEED
-		velocity.x = move_vector.x * WALK_SPEED
+		if Input.is_key_pressed(KEY_SHIFT):
+			current_walk_speed = WALK_SPEED * SPRINT_MULTIPLIER
+			$AnimationTree.set("parameters/conditions/run", true)
+			$AnimationTree.set("parameters/conditions/walk", false)
+		else:
+			current_walk_speed = WALK_SPEED
+			$AnimationTree.set("parameters/conditions/walk", true)
+			$AnimationTree.set("parameters/conditions/run", false)
+		velocity.z = move_vector.z * current_walk_speed
+		velocity.x = move_vector.x * current_walk_speed
 
 		var lookdir: float = atan2(-velocity.x, -velocity.z)
 		$Armature.rotation.y = lerp_angle($Armature.rotation.y, lookdir, 0.1)
@@ -38,6 +52,7 @@ func move(delta: float) -> void:
 		velocity.z = 0
 		$AnimationTree.set("parameters/conditions/idle", true)
 		$AnimationTree.set("parameters/conditions/walk", false)
+		$AnimationTree.set("parameters/conditions/run", false)
 		
 	# Need to 
 	if is_on_wall_only():
@@ -49,10 +64,11 @@ func move(delta: float) -> void:
 			velocity.y = (Vector3() + get_gravity()*2 * delta).y #Only affect the player's Y value, or it'll prevent them from moving off the wall without jumping
 			
 		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and (is_on_floor() or jump_time < 0.1):
 		jump()
+		print("Griddy")
 
 	move_and_slide()
 	
 func jump() -> void:
-	velocity.y += JUMP_VELOCITY
+	velocity.y = JUMP_VELOCITY
