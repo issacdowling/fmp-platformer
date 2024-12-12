42e7e51a4d5b724b76d8789d93d465c3afa6aa20
issacdowling <git@issacdowling.com>
2024-12-12 12:28:51 +0000

    Fix wall jumping, regular jumping, and many other movement bugs

--- player/player.gd
+++ player/player.gd
@@ -8,8 +8,10 @@ extends CharacterBody3D
 @export var MOUSE_LOOK_SENSITIVITY: int = 1
 @export var CONTROLLER_LOOK_SENSITIVITY: int = 1
 
-var jump_time: float = 0
+var air_time: float = 0
 var current_walk_speed: float
+var wall_time: float = 0
+var time_since_wall: float = 0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -24,18 +26,25 @@ func _physics_process(delta: float) -> void:
 	move(delta)
 
 func move(delta: float) -> void:
-	# Add gravity
-	if not is_on_floor() and not is_on_wall():
-		jump_time += delta / 1.5
-		velocity += (get_gravity() * delta) + (get_gravity() * jump_time * 0.25)
-	else:
-		jump_time = 0
+	## Apply gravity when in the air
+	if not is_on_floor():
+		_do_gravity(delta)
+	
+	## clear jump time when on ground or wall
+	if is_on_floor() or is_on_wall():
+		air_time = 0
+		
+	# Floor jumping
+	if not is_on_wall() and Input.is_action_pressed("jump") and (is_on_floor_only() or (air_time < 0.1)):
+		jump()
 		
 	# Get input, and move by input vector multiplied by the current transform	
 	var input_dir := Input.get_vector("left", "right", "forwards", "backwards")
 	# Unlike the original example, I DO NOT want this normalised, as analogue inputs should be analogue-ly usable
 	var move_vector := (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
-	if move_vector:
+
+	if move_vector and time_since_wall <= 0.0 and not is_on_wall_only():
+		# Make sure to set appropriate animations AND DISABLE THE OTHERS
 		$AnimationTree.set("parameters/conditions/idle", false)
 		if Input.is_action_pressed("sprint"):
 			current_walk_speed = WALK_SPEED * SPRINT_MULTIPLIER
@@ -49,14 +58,17 @@ func move(delta: float) -> void:
 		# Make "forwards" the direction that the camera's facing
 		move_vector = move_vector.rotated(Vector3.UP, get_viewport().get_camera_3d().rotation.y)
 		
+		# Move
 		velocity.z = move_vector.z * current_walk_speed
 		velocity.x = move_vector.x * current_walk_speed
 
+		# Look in direction of movement
 		var lookdir: float = atan2(-velocity.x, -velocity.z)
 		$Armature.rotation.y = lerp_angle($Armature.rotation.y, lookdir, 0.1)
 
 
-	elif not move_vector and is_on_floor(): # Stop when not pressing move buttons and grounded (THIS was stopping my wall jumps!)
+	# Stop when not pressing move buttons and grounded (THIS was stopping my wall jumps!)
+	elif not move_vector and (is_on_floor() or is_on_wall()): 
 		#TODO: Add a slippiness variable that impacts how quickly you slow down.
 		velocity.x = 0
 		velocity.z = 0
@@ -64,20 +76,34 @@ func move(delta: float) -> void:
 		$AnimationTree.set("parameters/conditions/walk", false)
 		$AnimationTree.set("parameters/conditions/run", false)
 		
-	# Need to 
+	
 	if is_on_wall_only():
-		if Input.is_action_just_pressed("jump"):
-			velocity.y += JUMP_VELOCITY
-			velocity.x += get_wall_normal().x * 5 # Move in the opposite direction that the wall is facing
-		elif not Input.is_action_pressed("jump"):
-			# This accidentally happens to enable wall-running. Keep this in as an "it's a feature, not a bug" thing?
-			velocity.y = (Vector3() + get_gravity()*2 * delta).y #Only affect the player's Y value, or it'll prevent them from moving off the wall without jumping
-			
+		wall_time += delta
+		# If we've been on the wall without jumping off for more than 1s, we should lose grip and slip down
+		if wall_time < 1:
+			if Input.is_action_just_pressed("jump"):
+				time_since_wall = 0.3
+				velocity.y += JUMP_VELOCITY
+				velocity += get_wall_normal() * 5 # Move in the opposite direction that the wall is facing
+				wall_time = 0
+			elif not Input.is_action_pressed("jump"):
+				# This accidentally happens to enable wall-running. Keep this in as an "it's a feature, not a bug" thing?
+				velocity.y = (Vector3() + get_gravity()*2 * delta).y #Only affect the player's Y value, or it'll prevent them from moving off the wall without jumping
+				#_do_gravity(delta)
+		else:
+			_do_gravity(delta)
+	else:
+		wall_time = 0
+		if time_since_wall >= 0:
+			time_since_wall -= delta
 		
-	if Input.is_action_pressed("jump") and (is_on_floor() or jump_time < 0.1):
-		jump()
+
 
 	move_and_slide()
+	
+func _do_gravity(delta: float) -> void:
+	air_time += delta / 1.5
+	velocity += (get_gravity() * delta) + (get_gravity() * air_time * 0.25)
 	
 func jump() -> void:
 	velocity.y = JUMP_VELOCITY
