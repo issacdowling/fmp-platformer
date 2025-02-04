7bb127a4ac1d3033ef44c2e8ef0d2f3adcd0d140
autobot27 <me@banudev.com>
2025-02-04 13:20:34 +0000

    CHORE: Began working on example enemy

--- reusables/enemy.gd
+++ reusables/enemy.gd
@@ -1,1 +1,9 @@
 extends Node
+
+@onready var area: Area3D = $EnemyArea
+
+func _ready() -> void:
+	area.area_entered.connect(_on_collision)
+	
+func _on_collision(area: Area3D) -> void:
+	print("collided")
