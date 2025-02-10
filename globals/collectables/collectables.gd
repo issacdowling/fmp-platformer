2f0b8f799e2bf8e9dd18e6bb5f08c0a6b98a22df
issacdowling <git@issacdowling.com>
2025-02-10 14:11:16 +0000

    Collectables HUD display timer resets on every new collection

--- globals/collectables/collectables.gd
+++ globals/collectables/collectables.gd
@@ -3,10 +3,13 @@ extends Node
 signal update(values: Dictionary[String, int])
 
 var collected: Dictionary[String, int] = {"Coin": 0, "Corn": 0}
+@export var popup_display_length_seconds: int = 3
+@onready var display_timer: Timer = $CollectableDisplayTimer
 
 # Ensure that the initial display is up-to-date
 func _ready() -> void:
 	update.emit(collected)
+	display_timer.timeout.connect(_on_hud_done)
 
 func report_collected(count: int, type: String) -> void:
 	assert(collected.has(type), "unrecognised collectable")
@@ -16,5 +19,7 @@ func report_collected(count: int, type: String) -> void:
 	update.emit(collected)
 	print(type, " collected")
 	Menu.show_collectables()
-	await get_tree().create_timer(3).timeout
+	display_timer.start(popup_display_length_seconds)
+
+func _on_hud_done() -> void:
 	Menu.hide_collectables()
