3878e371583c4dda9f1dc2f0f466aac56f58b0db
issacdowling <git@issacdowling.com>
2025-02-13 13:01:33 +0000

    FIX: objects that update in-editor no longer erroneously save their positions, all warnings dealt with

--- reusables/collectables/collectable.gd
+++ reusables/collectables/collectable.gd
@@ -8,8 +8,10 @@ class_name Collectable
 @export var degress_per_second: float = 10
 
 @onready var collectable_area: Area3D = $CollectableArea
+var initial_rotation: Vector3
 
 func _ready() -> void:
+	initial_rotation = self.rotation
 	collectable_area.area_entered.connect(_on_collected)
 	
 func _on_collected(area: Area3D) -> void:
@@ -19,4 +21,11 @@ func _on_collected(area: Area3D) -> void:
 
 func _process(delta: float) -> void:
 	rotate_y(degress_per_second * delta)
-	
+
+# Ensure @tool rotation previews aren't being saved
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_EDITOR_PRE_SAVE:
+		set_process(false)
+		self.rotation = initial_rotation
+	elif what == NOTIFICATION_EDITOR_POST_SAVE:
+		set_process(true)
