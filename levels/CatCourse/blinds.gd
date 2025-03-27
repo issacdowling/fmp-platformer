f497edc270818f2bd01df97d35c3d4c8e7de370f
issacdowling <git@issacdowling.com>
2025-03-27 14:42:07 +0000

    CONTENT: Multiple switches to control blind in cat course

--- levels/CatCourse/blinds.gd
+++ levels/CatCourse/blinds.gd
@@ -8,18 +8,20 @@ var initial_position: Vector3
 var end_position: Vector3
 var up: bool = false
 @onready var player: Player = %Player
-@onready var switch: Node3D = $switch
+@onready var top_switch: Node3D = $"../TopSwitch"
+@onready var bottom_switch: Node3D = $"../BottomSwitch"
 @export var open_movement_units: float = 9
 
 func _ready() -> void:
 	initial_position = self.global_position
 	end_position = initial_position + Vector3.UP * open_movement_units
 	
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and player.global_position.distance_to(switch.global_position) < 3:
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and (player.global_position.distance_to(top_switch.global_position) < 3 or player.global_position.distance_to(bottom_switch.global_position) < 3):
 		up = !up
-		switch.rotate(Vector3.UP, deg_to_rad(180)) # You can see it rotating, which is awkward
+		top_switch.rotate(Vector3.FORWARD, deg_to_rad(180)) # You can see it rotating, which is awkward
+		bottom_switch.rotate(Vector3.FORWARD, deg_to_rad(180))
 	if up:
-		self.global_position = self.global_position.lerp(end_position, 0.01)
+		self.global_position = self.global_position.lerp(end_position, 0.5 * delta)
 	else:
-		self.global_position =  self.global_position.lerp(initial_position, 0.01)
+		self.global_position =  self.global_position.lerp(initial_position, 0.5 * delta)
