d67b2575f55f2071af73b3dee7262ebb49337461
issacdowling <git@issacdowling.com>
2025-04-22 23:34:21 +0100

    CHORE: Make projectiles use attack signals instead of polling

--- reusables/enemies/turret/projectile.gd
+++ reusables/enemies/turret/projectile.gd
@@ -5,28 +5,29 @@ extends Node3D
 @onready var shot_back_area: Area3D = $ShotBackArea
 @onready var particle_emitter: GPUParticles3D = $GPUParticles3D
 @onready var mesh: MeshInstance3D = $Mesh
-var player: Player # This is manually set by the turret, not to be initialised here.
+
+# This is manually set by the turret, not to be initialised here (weird scene stuff)
+# However, that also means I can't connect signals in _ready, so I do them with a setter
+var player: Player: 
+	set(value):
+		player = value
+		player.attack.connect(_on_hit)
+
 var has_been_hit: bool = false
 
 var expiration_counter: float = 4
+
+const player_attack_distance: float = 3
 
 func _ready() -> void:
 	particle_emitter.visible = true
 	particle_emitter.emitting = false
 	damage_area.area_entered.connect(_on_damage_collision)
 
-
-func _process(delta: float) -> void:
-	expiration_counter -= delta
-	if expiration_counter <= 0:
-		queue_free()
-
-	translate(Vector3.FORWARD * delta * speed)
-
-		
-	# If the player hits the projectile broadly back in the direction of the turret, it should go straight back to it
-	# If not, it should still fly away
-	if !has_been_hit and player.currently_attacking and player.area in shot_back_area.get_overlapping_areas():
+# If the player hits the projectile broadly back in the direction of the turret, it should go straight back to it
+# If not, it should still fly away
+func _on_hit() -> void:
+	if !has_been_hit and player.global_position.distance_to(self.global_position) < player_attack_distance:
 		print("Player attacked projectile")
 		has_been_hit = true
 		if player.last_non_zero_move_vector.angle_to(global_transform.basis.z) <= deg_to_rad(25) and player.last_non_zero_move_vector.angle_to(global_transform.basis.z) >= deg_to_rad(-25):
@@ -42,7 +43,14 @@ func _process(delta: float) -> void:
 			
 			await get_tree().create_timer(particle_emitter.lifetime).timeout
 			queue_free()
-	
+
+
+func _process(delta: float) -> void:
+	expiration_counter -= delta
+	if expiration_counter <= 0:
+		queue_free()
+	translate(Vector3.FORWARD * delta * speed)
+
 
 func _on_damage_collision(other_area: Area3D) -> void:
 	if other_area.is_in_group("player"):
