61c26cd945510de69123332266109cb4288a3277
issacdowling <git@issacdowling.com>
2025-05-01 13:53:56 +0100

    CHORE: Make projectiles only hittable from closer

--- reusables/enemies/turret/projectile.gd
+++ reusables/enemies/turret/projectile.gd
@@ -6,6 +6,8 @@ extends Node3D
 @onready var particle_emitter: GPUParticles3D = $GPUParticles3D
 @onready var mesh: MeshInstance3D = $Mesh
 
+var has_exploded: bool = false
+
 # This is manually set by the turret, not to be initialised here (weird scene stuff)
 # However, that also means I can't connect signals in _ready, so I do them with a setter
 var player: Player: 
@@ -15,7 +17,7 @@ var player: Player:
 
 var has_been_hit: bool = false
 var expiration_counter: float = 4
-const player_attack_distance: float = 3
+const player_attack_distance: float = 2
 
 func _ready() -> void:
 	particle_emitter.visible = true
@@ -65,6 +67,13 @@ func _on_damage_collision(other_area: Area3D) -> void:
 		queue_free()
 
 func _explode() -> void:
+	# We return early to prevent multiple _explode() calls from causing issues,
+	# but still error if in the editor to help debugging
+	if has_exploded:
+		push_error("should not explode twice, but did")
+		return
+	
+	has_exploded = true
 	damage_area.area_entered.disconnect(_on_damage_collision)
 	damage_area.body_entered.disconnect(_on_obstacle_collision)
 	particle_emitter.emitting = true
