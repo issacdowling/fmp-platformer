54a36e9f682495d7e9744cd588dc38c4dca64b77
issacdowling <git@issacdowling.com>
2025-05-08 11:23:50 +0100

    CONTENT: Begin working on the next side of TopsyTurvy, implement rotation using timer
    At the moment, every side of the level has the same amount of time to be
    completed, and that timer finishing means the level rotates to the next
    side. I still don't have a way to stop the player from cheating their
    way to the end...

--- levels/TopsyTurvy/main_ground.gd
+++ levels/TopsyTurvy/main_ground.gd
@@ -2,8 +2,22 @@ extends Node3D
 
 @onready var player: Player = %Player
 
+var target_angle: int = 0
+var section_length: int = 40
+
 func _ready() -> void:
-	player.interact.connect(_spin)
+	Menu.rotate_timer_done.connect(_spin)
+	self.rotation.z = 0
+	Menu.do_rotate_timer(section_length)
+	print("test")
 
 func _spin() -> void:
-	rotate_z(deg_to_rad(90))
+	target_angle += 90
+	Menu.do_rotate_timer(section_length)
+	return
+
+
+# Slight rotation to further explain to the player what's going to happen
+func _process(delta: float) -> void:
+	#rotate_z(deg_to_rad(0.1 * delta))
+	self.rotation.z = lerp_angle(self.rotation.z, deg_to_rad(target_angle), 3 * delta)
